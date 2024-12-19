25700e4461af3a8af7ff744b8da501667962bdc4
TrilochanprasadBH <tptrilochanprasad@gmail.com>
2024-12-19 12:19:20 +0530

    hero heading css added

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,12 +9,12 @@ import UserMenu from "@/components/user-menu";
 function Header(props) {
     return (
         <header className={'flex flex-col gap-5 items-center py-10   sm:flex-row justify-between  py-6 px-6'}>
-            <h1 className={'text-4xl text-white-500'}>XrumBoard</h1>
+            <h1 className={'logo-text'}>XrumBoard</h1>
 
             <nav>
                 {/*<Image src={} alt={}/>*/}
 
-                <div className={'flex justify-between items-center  gap-4'}>
+                <div className={'flex justify-between items-center gap-4 '}>
                     <Link href="/project/create">
                         <Button variant={'destructive'}>
                             <PenBox size={'small'}/>
