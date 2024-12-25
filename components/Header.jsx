c8cefded29ca4213d1cff7c056d1547af9600086
TrilochanprasadBH <tptrilochanprasad@gmail.com>
2024-12-25 18:55:30 +0530

    connected db, checked if user exists/ loggedin/createdNewUser

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,8 +5,11 @@ import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import UserMenu from "@/components/user-menu";
+import {checkUser} from "@/lib/checkUser";
 
-function Header(props) {
+async function Header(props) {
+    await checkUser();
+//wrote above line of await only after checkUser.js code was written
     return (
         <header className={'flex flex-col gap-5 items-center py-10   sm:flex-row justify-between  py-6 px-6'}>
             <h1 className={'logo-text'}>XrumBoard</h1>
